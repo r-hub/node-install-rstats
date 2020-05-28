a9e8aad0dae87ba0055ffa87c6cb905247542904
Gábor Csárdi <csardi.gabor@gmail.com>
2020-05-28 23:47:12 +0100

    Add CLI

---
+++ install-r.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+const meow = require('meow');
+const installr = require('.');
+
+const cli = meow(`
+	Usage
+	  $ install-r [rversion1] [rversion2] ...
+
+	Supported R versions:
+	  'release':   the current R release
+	  'oldrel':    the previous minor branch
+	  'x.y':       last released version of a minor branch
+	  'x.y.z':     version x.y.z
+`);
+
+installr.install(cli.input);
